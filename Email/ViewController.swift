5ee2a7ae4789ec3268210383e5694aaea6ffaeab
BlissPonselvi <ponselvi@blisslogix.net>
2016-03-22 11:14:01 +0530

    33

---
+++ Email/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Email
+//
+//  Created by S.Ponselvi Christo on 18/03/16.
+//  Copyright © 2016 S.Ponselvi Christo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIAlertViewDelegate {
+
+    @IBOutlet var name: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    func isValidEmail(testStr:String) -> Bool {
+        
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        
+        return emailTest.evaluateWithObject(testStr)
+        
+        
+    }
+    
+    @IBAction func Submit(sender: AnyObject) {
+        
+        if (isValidEmail(name.text!)){
+            
+            let alert : UIAlertView = UIAlertView(title: "Alert", message: "valid Login", delegate: self, cancelButtonTitle: "OK");
+            
+            alert.show()
+            
+            
+        }else{
+            
+            let alert : UIAlertView = UIAlertView(title: "Alert", message: "Invalid Login", delegate: self, cancelButtonTitle: "OK");
+            
+            alert.show()
+            
+            
+        }
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
